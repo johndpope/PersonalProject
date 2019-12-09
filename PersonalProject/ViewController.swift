87f82d966053ee69bff041557e46e44900c5f9c5
Robbe Verhoest <robbe.verhoest@student.howest.be>
2019-12-09 14:43:59 +0100

    ar plane guidance + mvp

--- PersonalProject/ViewController.swift
+++ PersonalProject/ViewController.swift
@@ -6,20 +6,50 @@
 //  Copyright © 2019 Robbe Verhoest. All rights reserved.
 //
 
+import ARKit
 import UIKit
 import RealityKit
 
 class ViewController: UIViewController {
     
-    //the app's root view
     @IBOutlet var arView: ARView!
+    @IBOutlet weak var overlayView: ARCoachingOverlayView!
+    @IBOutlet weak var startGameButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
     
-    //button that start or restarts the game
-    @IBOutlet weak var gameActionButton: UIButton!
+    var gameAnchor:Experience.Game!
     
+    let coachingOverlay = ARCoachingOverlayView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        coachingOverlayViewWillActivate(overlayView)
+        setupCoachingOverlay()
+        coachingOverlayViewDidDeactivate(overlayView)
         
+        gameAnchor = try! Experience.loadGame()
+                
+        gameAnchor.notifications.hideAtStart.post()
     }
+    
+    @IBAction func startGame(_ sender: UIButton) {
+        startGameButton.isHidden = true
+        playButton.isHidden = false
+        pauseButton.isHidden = false
+        
+        arView.scene.anchors.append(gameAnchor)
+        
+        gameAnchor.notifications.gameStart.post()
+    }
+    
+    @IBAction func makeBridge(_ sender: UIButton) {
+        gameAnchor.notifications.extrudeBridge.post()
+    }
+    
+    @IBAction func pauseAnimation(_ sender: UIButton) {
+        gameAnchor.bridge?.stopAllAnimations(recursive: true)
+        gameAnchor.notifications.pauseAndRotate.post()
+    }
+    
 }
